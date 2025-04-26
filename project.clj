f7ee645a61b1dfc1a6b2dbbde8d15f74efbbfb8e
Nathan Marz <nathan@redplanetlabs.com>
2025-04-26 08:41:25 -1000

    fix compilation

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
 (defproject warzone "0.0.1-SNAPSHOT"
-  :source-path "src/clj"
-  :test-path "test/clj"
-  :java-source-path "src/jvm"
-  :java-test-path "test/jvm"
-  :javac-options {:debug "true" :fork "true"}
+  :java-source-paths ["src/jvm"]
   :dependencies []
   :dev-dependencies [])
